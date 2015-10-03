7e6717843a8acb73368c99fb7911d0970a55b5fc
Björn Bartels <bba@p-ad.de>
2015-10-03 23:00:52 +0200

    update assets install scripts

--- shell/files.themes.sh
+++ shell/files.themes.sh
@@ -1,7 +1,7 @@
 mkdir -p public/assets/themes/taurus
 rm -rf public/assets/themes/taurus/*
-cp -r vendor/themes/zf2-taurus/asstets* public/assets/themes/taurus/
+cp -r vendor/themes/zf2-taurus/assets/* public/assets/themes/taurus/
 
 mkdir -p public/assets/themes/remark
 rm -rf public/assets/themes/remark/*
-cp -r vendor/themes/zf2-remark/assets* public/assets/themes/remark/
+cp -r vendor/themes/zf2-remark/assets/* public/assets/themes/remark/
