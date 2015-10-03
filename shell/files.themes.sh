64604b9faec19436ef8890936554af0646bb53cd
Björn Bartels <bba@p-ad.de>
2015-10-04 01:47:02 +0200

    update themes install script

--- shell/files.themes.sh
+++ shell/files.themes.sh
@@ -1,7 +1,13 @@
 mkdir -p public/assets/themes/taurus
+mkdir -p themes/taurus
 rm -rf public/assets/themes/taurus/*
 cp -r vendor/themes/zf2-taurus/assets/* public/assets/themes/taurus/
+cp -r vendor/themes/zf2-taurus/view themes/taurus/
+#rm -rf vendor/themes/zf2-taurus
 
 mkdir -p public/assets/themes/remark
+mkdir -p themes/remark
 rm -rf public/assets/themes/remark/*
 cp -r vendor/themes/zf2-remark/assets/* public/assets/themes/remark/
+cp -r vendor/themes/zf2-remark/view themes/remark/
+#rm -rf vendor/themes/zf2-taurus
