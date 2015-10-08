4b8f248005b2230861afbe8a15e2bce69db40a6c
Björn Bartels <bba@p-ad.de>
2015-10-08 17:27:36 +0200

    update composer info

--- shell/build.jenkins.sh
+++ shell/build.jenkins.sh
@@ -1,4 +1,4 @@
-export COMPOSER_CACHE_DIR=/tmp/compser/
+export COMPOSER_CACHE_DIR=/tmp/composer/
 php /usr/bin/composer.phar clear-cache
 php /usr/bin/composer.phar self-update
 php /usr/bin/composer.phar install
