e26e108d6a32b75aaadd5f56f486cb61dacde0ae
Björn Bartels <bba@p-ad.de>
2015-10-08 15:02:38 +0200

    update jenkins build script

--- shell/build.jenkins.sh
+++ shell/build.jenkins.sh
@@ -1,6 +1,4 @@
 export COMPOSER_CACHE_DIR=/tmp/
 php /usr/bin/composer.phar self-update
 php /usr/bin/composer.phar install
-chmod -R ugoa+rx ./files
-./shell/files.components.sh
-./shell/files.themes.sh
+php /usr/bin/composer.phar update
