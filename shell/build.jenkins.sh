1da9a032b5bbd388d598a37bf194ef9929cf015b
Björn Bartels <bba@p-ad.de>
2015-10-08 16:16:54 +0200

    update jenkins script

--- shell/build.jenkins.sh
+++ shell/build.jenkins.sh
@@ -1,4 +1,5 @@
 export COMPOSER_CACHE_DIR=/tmp/
+php /usr/bin/composer.phar clear-cache
 php /usr/bin/composer.phar self-update
 php /usr/bin/composer.phar install
 php /usr/bin/composer.phar update
