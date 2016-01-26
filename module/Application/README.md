92991375268b98f8f94691568efb59566b3fe3f1
Björn Bartels <bba@p-ad.de>
2016-01-26 13:42:27 +0100

    update the files' info header remove obsolete 'readme'

--- module/Application/README.md
+++
@@ -1,3 +0,0 @@
-### Application Module
-
-	main ZendFramwork2 application module
--- public/index.php
+++ public/index.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * BB's Zend Framework 2 Components
+ * 
+ * BaseApp
+ *
+ * @package		[MyApplication]
+ * @package		BB's Zend Framework 2 Components
+ * @package		BaseApp
+ * @author		Björn Bartels [dragon-projects.net] <info@dragon-projects.net>
+ * @link		http://gitlab.dragon-projects.de:81/groups/zf2
+ * @license		http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @copyright	copyright (c) 2016 Björn Bartels [dragon-projects.net] <info@dragon-projects.net>
+ */
+
 ini_set('error_reporting', E_ALL);
 //ini_set('error_log', '/var/www/vhosts/system/dev.swim-db.de/logs/error_log');
 ini_set('log_errors', 'On');
