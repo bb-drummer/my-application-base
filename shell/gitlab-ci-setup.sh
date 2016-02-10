0e2254158f4eab05f3b7f7ab31d5455ec7a0512d
Björn Bartels <bba@p-ad.de>
2016-02-10 15:11:34 +0100

    update CI config

--- shell/gitlab-ci-setup.sh
+++ shell/gitlab-ci-setup.sh
@@ -12,13 +12,13 @@ apt-get update -yqq
 apt-get install git -yqq
 
 # Install phpunit, the tool that we will use for testing
-curl -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
+#curl -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
+#chmod +x /usr/local/bin/phpunit
 
 # Install mysql driver
 # Here you can install any other extension that you need
 apt-get update -y
-apt-get install -y php5-mysql
+apt-get install -y phpunit php5-mysql
 
 
 # Install composer
