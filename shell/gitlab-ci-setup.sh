8587112063f5c9bbd7f269a4381b734762c5a5b3
Björn Bartels <bba@p-ad.de>
2016-02-11 03:00:16 +0100

    update CI config

--- shell/gitlab-ci-setup.sh
+++ shell/gitlab-ci-setup.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#PATH='$PATH:/usr/local/bin'
-
-#echo 'current paths: $PATH'
-
-# We need to install dependencies only for Docker
-#[[ ! -e /.dockerinit ]] && exit 0
-
-#set -xe
-ls -la
-
 # php info
 apt-get update -yqq
 apt-get install -yqq php5-mysql
@@ -18,18 +8,14 @@ php -m
 
 # Install git (the php image doesn't have it) which is required by composer
 # Install mysql driver
-apt-get update -yqq
-apt-get install -yqq git
+apt-get update -y
+apt-get install -y git
+
 # Install composer
 curl -sS https://getcomposer.org/installer | php
 php composer.phar --version
-# Install all project dependencies
-#php -d memory_limit=-1 composer.phar install
-#php -d memory_limit=-1 composer.phar update
 
 # Install phpunit, the tool that we will use for testing
-apt-get install -yqq phpunit
+apt-get install -y phpunit
 curl -o ./phpunit.phar https://phar.phpunit.de/phpunit.phar
 chmod +x ./phpunit.phar
-
-
