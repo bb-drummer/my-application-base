98cd49b4b703a8da94dea1a255473be57999a48e
Björn Bartels <bba@p-ad.de>
2016-02-26 11:44:33 +0100

    update SQL schema and demo data

--- sql/install.sql
+++ sql/install.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `acl` (
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`acl_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS `aclresource` (
   `aclresources_id` bigint(10) NOT NULL AUTO_INCREMENT,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS `aclresource` (
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`aclresources_id`),
   UNIQUE KEY `resourceslug` (`resourceslug`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS `aclrole` (
   `aclroles_id` bigint(10) NOT NULL AUTO_INCREMENT,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `aclrole` (
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`aclroles_id`),
   UNIQUE KEY `roleslug` (`roleslug`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS `applications` (
   `application_id` bigint(10) NOT NULL AUTO_INCREMENT,
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `applications` (
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`application_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS `clients` (
   `clients_id` bigint(10) NOT NULL AUTO_INCREMENT,
@@ -62,7 +62,22 @@ CREATE TABLE IF NOT EXISTS `clients` (
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`clients_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `settings_id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `scope` varchar(255) COLLATE utf8_bin NOT NULL,
+  `ref_id` bigint(10) NOT NULL,
+  `type` varchar(32) COLLATE utf8_bin NOT NULL,
+  `name` varchar(32) COLLATE utf8_bin NOT NULL,
+  `value` text COLLATE utf8_bin NOT NULL,
+  `application_id` bigint(10) NOT NULL DEFAULT '0',
+  `client_id` bigint(10) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`settings_id`),
+  UNIQUE KEY `type_name` (`type`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` bigint(10) NOT NULL AUTO_INCREMENT,
@@ -81,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS `userprofile` (
   `user_id` bigint(10) NOT NULL AUTO_INCREMENT,
@@ -99,13 +114,67 @@ CREATE TABLE IF NOT EXISTS `userprofile` (
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE IF NOT EXISTS `user_provider` (
   `user_id` bigint(10) NOT NULL,
-  `provider_id` varchar(50) NOT NULL,
-  `provider` varchar(255) NOT NULL,
+  `provider_id` varchar(50) COLLATE utf8_bin NOT NULL,
+  `provider` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`user_id`,`provider_id`),
-  UNIQUE KEY (`provider_id`,`provider`),
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB;
+  UNIQUE KEY `provider_id` (`provider_id`,`provider`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+--
+-- Demo-Daten für Demo-Installation
+--
+
+--
+-- Daten für Tabelle `aclresource`
+--
+
+INSERT INTO `aclresource` (`aclresources_id`, `resourceslug`, `resourcename`, `application_id`, `client_id`, `created`, `modified`) VALUES
+(1, 'mvc:nouser', 'kein Benutzer', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'mvc:user', 'Benutzer', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 'mvc:admin', 'Administration', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
+-- Daten für Tabelle `aclrole`
+--
+
+INSERT INTO `aclrole` (`aclroles_id`, `roleslug`, `rolename`, `application_id`, `client_id`, `created`, `modified`) VALUES
+(1, 'admin', 'Administrator', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(4, 'public', 'kein Benutzer', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(6, 'user', 'Benutzer', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
+-- Daten für Tabelle `acl`
+--
+
+INSERT INTO `acl` (`acl_id`, `aclroles_id`, `aclresources_id`, `state`, `application_id`, `client_id`, `created`, `modified`) VALUES
+(1, 4, 1, 'allow', 0, 0, '0000-00-00 00:00:00', '2016-02-07 20:33:35'),
+(3, 1, 1, 'deny', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(47, 4, 4, 'deny', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(61, 1, 4, 'allow', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(62, 1, 2, 'allow', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(63, 4, 2, 'deny', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(64, 6, 4, 'deny', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(65, 6, 1, 'deny', 0, 0, '0000-00-00 00:00:00', '2016-02-26 01:46:24'),
+(66, 6, 2, 'allow', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`user_id`, `display_name`, `username`, `email`, `street`, `city`, `phone`, `password`, `state`, `aclrole`, `token`, `application_id`, `client_id`, `created`, `modified`) VALUES
+(1, 'System-Administrator', 'sysadmin', 'webmaster@myapplication.tld', '', '', '', '$2y$14$aNr/FPiS.Kbw8/ZYiOaXpum/4RrX7AGMaxA5vV6OIHUi1kr.w6GgC', 1, 'admin', '', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+
+--
+-- Daten für Tabelle `settings`
+--
+
+INSERT INTO `settings` (`settings_id`, `scope`, `ref_id`, `type`, `name`, `value`, `application_id`, `client_id`, `created`, `modified`) VALUES
+(1, '', 0, 'system', 'lang', 'de_DE', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, '', 0, 'user', 'lang', 'de_DE', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(7, 'application', 0, 'app', 'var', 'value test', 0, 0, '0000-00-00 00:00:00', '2016-01-29 20:37:59');
