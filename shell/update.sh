ca5a470bf55afd61d718fbfc88636740a0fbf7f4
Björn Bartels <bba@p-ad.de>
2016-02-26 12:19:15 +0100

    add simple update shell script

---
+++ shell/update.sh
@@ -0,0 +1,24 @@
+#
+# simple project update script
+#
+clear
+
+#
+# pull current state from repository
+#
+git pull; 
+
+#
+# composer/dependencies updates
+#
+php -d memory_limit=-1 composer.phar update; 
+
+#
+# database updates
+#
+php -d memory_limit=-1 public/index.php update-db -v; 
+
+#
+# test coverage
+#
+vendor/bin/phpunit --verbose --colors --debug --configuration ./module/Application/test/phpunit.xml --coverage-text;
