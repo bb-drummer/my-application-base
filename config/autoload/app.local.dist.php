0eb72d18fae1b7df2677659967fc0b4aeddce6ec
Björn Bartels <bba@p-ad.de>
2016-03-02 15:29:46 +0100

    again, set ZF2 back to v2.5 instead of dev-master, add (sample) cache config

--- config/autoload/app.local.dist.php
+++ config/autoload/app.local.dist.php
@@ -30,6 +30,38 @@ return array(
 	),
 		
 	/**
+	 * cache configuration
+	 **/
+	'caches' => array(
+    	'CacheService' => array(
+            'adapter' => array(
+                'name'     =>'memcached',
+                'ttl'      => 3600,
+                'lifetime' => 3600,
+                'options'  => array(
+                    'servers'   => array(
+                        array(
+                            '127.0.0.1', 11211
+                        )
+                    ),
+                    'namespace'  => 'my-application',
+                    'liboptions' => array (
+                        'COMPRESSION'     => true,
+                        'binary_protocol' => true,
+                        'no_block'        => true,
+                        'connect_timeout' => 100
+                    )
+                )
+            ),
+            'plugins' => array(
+                'exception_handler' => array(
+                    'throw_exceptions'    => false
+                ),
+            ),
+        ),
+	),
+		
+	/**
 	 * SMTP configuration
 	 **/
 	'zfcuser_smtp' => array(
